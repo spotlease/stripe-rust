123b617fbd0a31f16e85288fac53966e2385a2be
Nick Cardin <nick@cardin.email>
2018-05-23 16:00:17 -0400

    add capture to charge

--- src/request_builders/charge.rs
+++ src/request_builders/charge.rs
@@ -1,4 +1,6 @@
 use models::{Charge, ChargeCreateParams, ChargeUpdateParams, ChargeListParams};
+use request::{Request, SimpleRequest};
+use reqwest::Method;
 
 use resx::{ResxInstanceRB, ResxPath, ResxRB};
 
@@ -9,5 +11,13 @@ impl<'a> super::StripeResourceRB<ChargeListParams<'a>, ChargeCreateParams<'a>, C
 
 #[derive(ResxPath, ResxInstanceRB)]
 pub struct ChargeRB(String);
+
+impl ChargeRB {
+    pub fn capture<'a>(mut self) -> SimpleRequest<Charge> {
+        
+        self.0.push_str("/capture");
+        Request::new(Method::Post, self.0)
+    }
+}
 
 impl<'a> super::StripeResourceInstanceRB<ChargeUpdateParams<'a>, Charge> for ChargeRB {}
